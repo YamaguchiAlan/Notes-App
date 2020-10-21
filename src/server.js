918cb6d05181b310d95669fea84e6ab3f7910ffb
YamaguchiAlan <alanyamaguchi72@gmail.com>
2020-10-21 13:08:16 -0300

    Primer commit

---
+++ src/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const morgan = require('morgan');
+
+// Initializations
+const app = express();
+
+// Setting
+app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+
+// Global Variables
+
+// Routes
+app.use(require('./routes/index.routes'));
+app.use(require('./routes/notes.routes'));
+
+// Static Files
+app.use(express.static(path.join(__dirname, 'public')));
+
+module.exports = app;
